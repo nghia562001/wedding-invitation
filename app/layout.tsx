694f4475b774553839211f50c667dd3b400b5bad
Trung Nghia <nghia562001@gmail.com>
2026-04-14 23:29:12 +0700

    fixbug

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,20 +9,30 @@ export const metadata: Metadata = {
     title: "Tường Vy & Trung Nghĩa",
     description: "Trân trọng kính mời bạn đến dự lễ cưới của chúng tôi.",
     type: "website",
+    images: [
+      {
+        url: "https://wedding-nghiavy.vercel.app/images/og_card.jpg", // 🔥 sửa lại domain của bạn
+        width: 1200,
+        height: 630,
+        alt: "Tường Vy & Trung Nghĩa",
+      },
+    ],
   },
 
+  // (khuyên dùng thêm cho Zalo + Twitter)
   twitter: {
-    card: "summary",
+    card: "summary_large_image",
     title: "Tường Vy & Trung Nghĩa",
     description: "Trân trọng kính mời bạn đến dự lễ cưới của chúng tôi.",
+    images: ["https://wedding-nghiavy.vercel.app/images/og_card.jpg"], // giống phía trên
   },
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="vi">
       <body>{children}</body>
